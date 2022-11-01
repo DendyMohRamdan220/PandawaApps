478a8396194925134a51b6594c49e906baac405c
DendyMohRamdan220 <dendymochramdan@gmai.com>
2022-11-01 18:22:01 +0700

    Update Logo

--- resources/views/layout/Client.blade.php
+++ resources/views/layout/Client.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Dashboard 2</title>
+    <title> Dashboard </title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -170,9 +170,10 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="index3.html" class="brand-link">
-                <img src="{{ asset('template/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <img src="{{ asset('template/dist/img/LogoPandawaMandiri.jpg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
 
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
+
+                <span class="brand-text font-weight-light">CV.Pandawa Mandiri</span>
             </a>
 
             <!-- Sidebar -->
@@ -180,11 +181,12 @@
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="{{ asset('template/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                        <img src="{{ asset('template/dist/img/ProfilDendy.jpg') }}" class="img-circle elevation-2" alt="User Image">
+
 
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">Alexander Pierce</a>
+                        <a href="#" class="d-block">Dendy Moh Ramdan</a>
                     </div>
                 </div>
 
@@ -240,29 +242,57 @@
                                 <i class="nav-icon fas fa-tachometer-alt"></i>
                                 <p>
                                     Dashboard<span class="right badge badge-danger">ERROR 404</span>
-
                                 </p>
                             </a>
                         </li>
 
                         <li class="nav-item">
                             <a href="/proyek" class="nav-link">
-                                <i class="nav-icon fas fa-tachometer-alt"></i>
+                                <i class="nav-icon fas fa-file-alt"></i>
                                 <p>
                                     Project<span class="right badge badge-danger">ERROR 404</span>
-
-
                                 </p>
                             </a>
                         </li>
 
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-dollar-sign"></i>
+                                <p>
+                                    Finance
+                                    <i class="right fas fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
+                                        {{-- <i class="far fa-circle nav-icon"></i> --}}
+                                        <p>Estimates</p>
+
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
+                                        {{-- <i class="far fa-circle nav-icon"></i> --}}
+                                        <p>Invoices</p>
+
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
+                                        {{-- <i class="far fa-circle nav-icon"></i> --}}
+                                        <p>Payments</p>
+
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
 
                         <li class="nav-item">
                             <a href="/tiket" class="nav-link">
                                 <i class="nav-icon fas fa-ticket-alt"></i>
                                 <p>
                                     Ticket<span class="right badge badge-danger">ERROR 404</span>
-
                                 </p>
                             </a>
                         </li>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,12 +7,12 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Dashboard v2</h1>
+                    <h1 class="m-0">Dashboard</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Dashboard v2</li>
+                        <li class="breadcrumb-item active">Dashboard</li>
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
@@ -242,7 +242,7 @@
             <!-- /.row -->
 
             <!-- Main row -->
-            <div class="row">
+            {{-- <div class="row">
                 <!-- Left col -->
                 <div class="col-md-8">
                     <!-- MAP & BOX PANE -->
@@ -868,7 +868,7 @@
                     <!-- /.card -->
                 </div>
                 <!-- /.col -->
-            </div>
+            </div> --}}
             <!-- /.row -->
         </div>
         <!--/. container-fluid -->
