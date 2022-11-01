be41b03c0cd08e6ae6d6b22aaf87f0f5acc4b084
DendyMohRamdan220 <dendymochramdan@gmai.com>
2022-11-01 19:59:12 +0700

    Update Menu Project

--- resources/views/layout/Client.blade.php
+++ resources/views/layout/Client.blade.php
@@ -306,6 +306,15 @@
                           </a>
                       </li>
 
+                      <li class="nav-item">
+                          <a href="#" class="nav-link">
+                              <i class="nav-icon fas fa-user-alt"></i>
+                              <p>
+                                  Project<span class="right badge badge-danger">ERROR 404</span>
+                              </p>
+                          </a>
+                      </li>
+
                         {{--
           <li class="nav-item">
             <a href="#" class="nav-link">
