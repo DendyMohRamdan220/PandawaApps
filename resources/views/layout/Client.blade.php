b71bba10dc37bdec8ea7a3e1c4bddd298ff27ac5
Ditya Ryanis <dityarys05@gmail.com>
2022-11-02 12:36:19 +0700

    Update Client.blade.php

--- resources/views/layout/Client.blade.php
+++ resources/views/layout/Client.blade.php
@@ -20,9 +20,9 @@
     <div class="wrapper">
 
         <!-- Preloader -->
-        <div class="preloader flex-column justify-content-center align-items-center">
+        {{-- <div class="preloader flex-column justify-content-center align-items-center">
             <img class="animation__wobble" src="{{ asset('template/dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60" width="60">
-        </div>
+        </div> --}}
 
         <!-- Navbar -->
         <nav class="main-header navbar navbar-expand navbar-dark">
