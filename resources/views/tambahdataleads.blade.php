87e8a15757aa9f9ed9d887d2373d03d1610e047d
Annisa Zachry Fauziah <annisazachry246@gmail.com>
2022-11-07 14:50:43 +0700

    Update tambah data lead part 1

--- resources/views/tambahdataleads.blade.php
+++ resources/views/tambahdataleads.blade.php
@@ -1,50 +1,81 @@
 @extends('layout.Client')
 
 @section('content')
-<body>
-    <h1 class="text-center mb-4">Add Leads</h1>
 
-    <div class="container">
+@push('css')
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+@endpush
 
-        <div class="row justify-content-center">
-            <div class="col-9">
-                <div class="card">
-                    <div class="card-body">
-                        <form action="/insertdatatiket" method="post" enctype="multipart/form-data">
-                            @csrf
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Ticket Subject</label>
-                                <input type="text" name="ticket_subject" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="">
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Description</label>
-                                <input type="text" name="description" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="">
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Other Detail</label>
-                                <br>
-                                <select class="form-select" name="others" aria-label="Default select example">
-                                    <option selected>Priority</option>
-                                    <option value="1">Low</option>
-                                    <option value="2">Medium</option>
-                                    <option value="3">High</option>
-                                    <option value="4">Urgent</option>
-                                </select>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </form>
-                    </div>
+<div class="content-wrapper">
+  <!-- /.content-header -->
+  <h4 class="mb-0 p-20 f-21 font-weight-normal text-capitalize border-bottom-grey">
+    Lead Details</h4>
+
+  <div class="container">
+    <div class="row p-20">
+      <div class="col-sm-12">
+            <form action="/insertdatatask" method="POST" enctype="multipart/form-data">
+              @csrf
+              <div class="col-lg-4 col-md-6">
+                <div class="form-group my-3">
+                    <label for="exampleInputEmail1" class="form-label">Lead Name</label>
+                    <input type="text" name="taskname" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                 </div>
-            </div>
+              </div>
+
+              <div class="col-lg-4 col-md-6">
+                <div class="form-group my-3">
+                    <label for="exampleInputEmail1" class="form-label">Lead Email</label>
+                    <input type="text" name="taskname" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <small id="client_emailHelp" class="form-text text-muted">Email will be used to send proposals.</small>
+                </div>
+              </div>
+
+              <div class="col-lg-4 col-md-6">
+                <div class="form-group my-3">
+                    <label for="exampleInputEmail1" class="form-label">Lead Name</label>
+                    <input type="text" name="taskname" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                </div>
+              </div>
+
+              <div class="col-lg-4 col-md-6">
+                <div class="form-group my-3">
+                    <label for="exampleInputEmail1" class="form-label">Lead Name</label>
+                    <input type="text" name="taskname" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                </div>
+              </div>
+
+              <div class="col-lg-4 col-md-6">
+                <div class="form-group my-3">
+                    <label for="exampleInputEmail1" class="form-label">Lead Name</label>
+                    <input type="text" name="taskname" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                </div>
+              </div>
+
+              <div class="col-lg-4 col-md-6">
+                <div class="form-group my-3">
+                    <label for="exampleInputEmail1" class="form-label">Lead Name</label>
+                    <input type="text" name="taskname" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                </div>
+              </div>
+
+              <div class="col-lg-4 col-md-6">
+                <div class="form-group my-3">
+                    <label for="exampleInputEmail1" class="form-label">Lead Name</label>
+                    <input type="text" name="taskname" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-6">
+                <label for="exampleInputEmail1" class="form-label">Status</label>
+                <input type="text" name="status" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              </div>
+
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+          </div>
         </div>
-    </div>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
-
+  </div>
+</div>
 @endsection
-
