643f0c9c95ef4b845eae815649377e412d37ebb2
DendyMohRamdan220 <dendymochramdan@gmai.com>
2022-11-23 12:25:03 +0700

    Update login.blade.php

--- resources/views/login_register/login.blade.php
+++ resources/views/login_register/login.blade.php
@@ -1,20 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="viho admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, viho admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="description"
+        content="viho admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+    <meta name="keywords"
+        content="admin template, viho admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="pixelstrap">
-    <link rel="icon" href="{{ asset('template') }}/assets/images/favicon.png" type="image/x-icon">
-    <link rel="shortcut icon" href="{{ asset('template') }}/assets/images/favicon.png" type="image/x-icon">
+    <link rel="icon" href="{{ asset('template/assets/images/logop.jpg') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('template/assets/images/logop.png') }}" type="image/x-icon">
     <title>Login - Pandawa Mandiri</title>
     <!-- Google font-->
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
+        rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;display=swap"
+        rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
+        rel="stylesheet">
     <!-- Font Awesome-->
     <link rel="stylesheet" type="text/css" href="{{ asset('template/assets/css/fontawesome.css') }}">
     <!-- ico-font-->
@@ -34,66 +43,87 @@
     <link id="color" rel="stylesheet" href="{{ asset('template/assets/css/color-1.css') }}" media="screen">
     <!-- Responsive css-->
     <link rel="stylesheet" type="text/css" href="{{ asset('template/assets/css/responsive.css') }}">
-  </head>
-  <body>
+</head>
+
+<body>
     <!-- Loader starts-->
     <div class="loader-wrapper">
-      <div class="theme-loader">
-        <div class="loader-p"></div>
-      </div>
+        <div class="theme-loader">
+            <div class="loader-p"></div>
+        </div>
     </div>
     <!-- Loader ends-->
     <!-- page-wrapper Start-->
     <section>
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-xl-7"><img class="bg-img-cover bg-center" src="{{ asset('template/assets/images/login/sans.jpg') }}" alt="looginpage"></div>
-          <div class="col-xl-5 p-0">
-            <div class="login-card">
-                <form class="theme-form login-form" method="POST" action="login_redirect">
-                  @csrf
-                  @if (session()->has('loginError'))
-                    {{ session('loginError') }}
-                  @endif
-                  <h4>Login</h4>
-                  <h6>Welcome back! Log in to your account.</h6>
-                  <div class="form-group">
-                    <label>Email Address</label>
-                    <div class="input-group"><span class="input-group-text"><i class="icon-email"></i></span>
-                      <input class="form-control" type="email" required="" name="email" placeholder="Email Address"
-                      id="exampleInputEmail1" value="{{ old('email') }}">
-                      @error('email')
-                        <strong>Warning!</strong>
-                        <span> {{ $message }} </span>
-                      @enderror
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-7"><img class="bg-img-cover bg-center"
+                        src="{{ asset('template/assets/images/login/sans.jpg') }}" alt="looginpage"></div>
+                <div class="col-xl-5 p-0">
+                    <div class="login-card">
+                        <form class="theme-form login-form" method="POST" action="login_redirect">
+                            @csrf
+                            @if (session()->has('loginError'))
+                                {{ session('loginError') }}
+                            @endif
+                            <h4>Login</h4>
+                            <h6>Welcome back! Log in to your account.</h6>
+                            <div class="form-group">
+                                <label>Email Address</label>
+                                <div class="input-group"><span class="input-group-text"><i
+                                            class="icon-email"></i></span>
+                                    <input class="form-control" type="email" required="" name="email"
+                                        placeholder="Email Address" id="exampleInputEmail1"
+                                        value="{{ old('email') }}">
+                                    @error('email')
+                                        <strong>Warning!</strong>
+                                        <span> {{ $message }} </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label>Password</label>
+                                <div class="input-group">
+                                    <span class="input-group-text">
+                                        <i class="icon-lock"></i>
+                                    </span>
+                                    <input class="form-control" type="password" name="password" required=""
+                                        placeholder="*********" id="pass">
+                                    <div class="input-group-append">
+
+                                        <!-- kita pasang onclick untuk merubah icon buka/tutup mata setiap diklik  -->
+                                        <span id="mybutton" onclick="change()" class="input-group-text">
+
+                                            <!-- icon mata bawaan bootstrap  -->
+                                            <svg width="2em" height="2em" viewBox="0 0 16 16"
+                                                class="bi bi-eye-fill" fill="currentColor"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                                                <path fill-rule="evenodd"
+                                                    d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+                                            </svg>
+                                        </span>
+                                    </div>
+                                    @error('password')
+                                        <strong>Warning!</strong>
+                                        <span> {{ $message }} </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="checkbox">
+                                    <input id="checkbox1" type="checkbox">
+                                    <label class="text-muted" for="checkbox1">Remember password</label>
+                                </div><a class="link" href="forget-password.html">Forgot password?</a>
+                            </div>
+                            <div class="form-group">
+                                <button class="btn btn-primary btn-block" type="submit">Sign in</button>
+                            </div>
+                        </form>
                     </div>
-                  </div>
-                  <div class="form-group">
-                    <label>Password</label>
-                    <div class="input-group"><span class="input-group-text"><i class="icon-lock"></i></span>
-                      <input class="form-control" type="password" name="password" required="" placeholder="*********"
-                      id="exampleInputPassword1">
-                      <div class="show-hide"><span class="show">                         </span></div>
-                      @error('password')
-                        <strong>Warning!</strong>
-                        <span> {{ $message }} </span>
-                      @enderror
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <div class="checkbox">
-                      <input id="checkbox1" type="checkbox">
-                      <label class="text-muted" for="checkbox1">Remember password</label>
-                    </div><a class="link" href="forget-password.html">Forgot password?</a>
-                  </div>
-                  <div class="form-group">
-                    <button class="btn btn-primary btn-block" type="submit">Sign in</button>
-                  </div>
-              </form>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </section>
     <!-- page-wrapper end-->
     <!-- latest jquery-->
@@ -113,5 +143,43 @@
     <script src="{{ asset('template/assets/js/script.js') }}"></script>
     <!-- login js-->
     <!-- Plugin used-->
-  </body>
+    <script>
+        // membuat fungsi change
+        function change() {
+
+            // membuat variabel berisi tipe input dari id='pass', id='pass' adalah form input password
+            var x = document.getElementById('pass')
+                .type;
+
+            //membuat if kondisi, jika tipe x adalah password maka jalankan perintah di bawahnya
+            if (x == 'password') {
+
+                //ubah form input password menjadi text
+                document.getElementById('pass')
+                    .type = 'text';
+
+                //ubah icon mata terbuka menjadi tertutup
+                document.getElementById('mybutton')
+                    .innerHTML = `<svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-eye-slash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                                    <path d="M10.79 12.912l-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z"/>
+                                                                    <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708l-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829z"/>
+                                                                    <path fill-rule="evenodd" d="M13.646 14.354l-12-12 .708-.708 12 12-.708.708z"/>
+                                                                    </svg>`;
+            } else {
+
+                //ubah form input password menjadi text
+                document.getElementById('pass')
+                    .type = 'password';
+
+                //ubah icon mata terbuka menjadi tertutup
+                document.getElementById('mybutton')
+                    .innerHTML = `<svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-eye-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                                    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+                                                                    <path fill-rule="evenodd" d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+                                                                    </svg>`;
+            }
+        }
+    </script>
+</body>
+
 </html>
