aba3a2697ad08719ccc6d66d831994c3bc3076d4
Ditya Ryanis <dityarys05@gmail.com>
2022-11-23 12:53:07 +0700

    Update layout.blade.php

--- resources/views/Layouts/layout.blade.php
+++ resources/views/Layouts/layout.blade.php
@@ -65,18 +65,42 @@
         <div class="page-main-header">
             <div class="main-header-right row m-0">
                 <div class="main-header-left">
+<<<<<<< HEAD
                     <div class="logo-wrapper"><a href="index.html"><img class="img-fluid"
                                 src="{{ asset('template/assets/images/logopm.jpg') }}" alt=""></a></div>
                     <div class="dark-logo-wrapper"><a href="index.html"><img class="img-fluid"
                                 src="{{ asset('template/assets/images/logopm.jpg') }}" alt=""></a></div>
                     <div class="toggle-sidebar"><i class="status_toggle middle" data-feather="align-center"
                             id="sidebar-toggle"></i></div>
+=======
+                    <div class="logo-wrapper">
+                        <a href="index.html">
+                            <img class="img-fluid" src="{{ asset('template/assets/images/logopm.jpg') }}"
+                                alt="">
+                        </a>
+                    </div>
+                    <div class="dark-logo-wrapper">
+                        <a href="index.html">
+                            <img class="img-fluid" src="{{ asset('template/assets/images/logopm.jpg') }}"
+                                alt="">
+                        </a>
+                    </div>
+                    <div class="toggle-sidebar">
+                        <i class="status_toggle middle" data-feather="align-center" id="sidebar-toggle">
+                        </i>
+                    </div>
+>>>>>>> ce5d8919c6790ea9cfbdb9ade883ade84c27f4f5
                 </div>
                 <div class="left-menu-header col">
                     <ul>
                         <li>
                             <form class="form-inline search-form">
+<<<<<<< HEAD
                                 <div class="search-bg"><i class="fa fa-search"></i>
+=======
+                                <div class="search-bg">
+                                    <i class="fa fa-search"></i>
+>>>>>>> ce5d8919c6790ea9cfbdb9ade883ade84c27f4f5
                                     <input class="form-control-plaintext" placeholder="Search here.....">
                                 </div>
                             </form><span class="d-sm-none mobile-search search-bg"><i class="fa fa-search"></i></span>
@@ -85,20 +109,38 @@
                 </div>
                 <div class="nav-right col pull-right right-menu p-0">
                     <ul class="nav-menus">
+<<<<<<< HEAD
                         <li><a class="text-dark" href="#!" onclick="javascript:toggleFullScreen()"><i
                                     data-feather="maximize"></i></a></li>
+=======
+                        <li>
+                            <a class="text-dark" href="#!" onclick="javascript:toggleFullScreen()">
+                                <i data-feather="maximize"></i>
+                            </a>
+                        </li>
+>>>>>>> ce5d8919c6790ea9cfbdb9ade883ade84c27f4f5
                         <li class="onhover-dropdown">
                             <div class="bookmark-box"><i data-feather="star"></i></div>
                             <div class="bookmark-dropdown onhover-show-div">
                                 <div class="form-group mb-0">
                                     <div class="input-group">
+<<<<<<< HEAD
                                         <div class="input-group-prepend"><span class="input-group-text"><i
                                                     class="fa fa-search"></i></span></div>
+=======
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">
+                                                <i class="fa fa-search">
+                                                </i>
+                                            </span>
+                                        </div>
+>>>>>>> ce5d8919c6790ea9cfbdb9ade883ade84c27f4f5
                                         <input class="form-control" type="text"
                                             placeholder="Search for bookmark...">
                                     </div>
                                 </div>
                                 <ul class="m-t-5">
+<<<<<<< HEAD
                                     <li class="add-to-bookmark"><i class="bookmark-icon"
                                             data-feather="inbox"></i>Email<span class="pull-right"><i
                                                 data-feather="star"></i></span></li>
@@ -111,16 +153,52 @@
                                     <li class="add-to-bookmark"><i class="bookmark-icon"
                                             data-feather="airplay"></i>Widgets<span class="pull-right"><i
                                                 data-feather="star"> </i></span></li>
+=======
+                                    <li class="add-to-bookmark">
+                                        <i class="bookmark-icon" data-feather="inbox">
+                                        </i>Email<span class="pull-right">
+                                            <i data-feather="star"></i>
+                                        </span>
+                                    </li>
+                                    <li class="add-to-bookmark">
+                                        <i class="bookmark-icon" data-feather="message-square">
+                                        </i>Chat<span class="pull-right">
+                                            <i data-feather="star"></i>
+                                        </span>
+                                    </li>
+                                    <li class="add-to-bookmark">
+                                        <i class="bookmark-icon" data-feather="command">
+                                        </i>Feather Icon<span class="pull-right">
+                                            <i data-feather="star"></i>
+                                        </span>
+                                    </li>
+                                    <li class="add-to-bookmark"><i class="bookmark-icon" data-feather="airplay">
+                                        </i>Widgets<span class="pull-right">
+                                            <i data-feather="star"> </i>
+                                        </span>
+                                    </li>
+>>>>>>> ce5d8919c6790ea9cfbdb9ade883ade84c27f4f5
                                 </ul>
                             </div>
                         </li>
                         <li class="onhover-dropdown">
+<<<<<<< HEAD
                             <div class="notification-box"><i data-feather="bell"></i><span
                                     class="dot-animated"></span></div>
                             <ul class="notification-dropdown onhover-show-div">
                                 <li>
                                     <p class="f-w-700 mb-0">You have 3 Notifications<span
                                             class="pull-right badge badge-primary badge-pill">4</span></p>
+=======
+                            <div class="notification-box"><i data-feather="bell"></i>
+                                <span class="dot-animated"></span>
+                            </div>
+                            <ul class="notification-dropdown onhover-show-div">
+                                <li>
+                                    <p class="f-w-700 mb-0">You have 3 Notifications
+                                        <span class="pull-right badge badge-primary badge-pill">4</span>
+                                    </p>
+>>>>>>> ce5d8919c6790ea9cfbdb9ade883ade84c27f4f5
                                 </li>
                                 <li class="noti-primary">
                                     <div class="media"><span class="notification-bg bg-light-primary"><i
@@ -262,12 +340,15 @@
                                             <li>
                                                 <a href="/attendance_admin">Attendance</a>
                                             </li>
+<<<<<<< HEAD
                                             <li>
                                                 <a href="/presensi_masuk">Absensi Masuk</a>
                                             </li>
                                             <li>
                                                 <a href="/presensi_keluar">Absensi Keluar</a>
                                             </li>
+=======
+>>>>>>> ce5d8919c6790ea9cfbdb9ade883ade84c27f4f5
                                         </ul>
                                     </li>
                                     <li class="dropdown">
